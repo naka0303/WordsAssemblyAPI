70fc3d990d57b12b7d26d0798f989a8cedb63203
naka0303 <jyunki.nakagawa@gmail.com>
2025-02-02 17:17:58 +0900

    first commit

---
+++ src/main/java/com/wordssassemblyapi/WordsAssemblyApiApplication.java
@@ -0,0 +1,13 @@
+package com.wordssassemblyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsAssemblyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordsAssemblyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wordssassemblyapi/WordsAssemblyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordssassemblyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsAssemblyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
