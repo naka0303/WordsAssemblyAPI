588766b655512e2910476d2710af297b2f6794c7
naka0303 <jyunki.nakagawa@gmail.com>
2025-03-16 00:37:13 +0900

    feature.ログイン機能実装

--- src/main/java/com/wordsassemblyapi/presentation/login/controller/LoginController.java
+++ src/main/java/com/wordsassemblyapi/presentation/login/controller/LoginController.java
@@ -5,7 +5,6 @@ import com.wordsassemblyapi.application.login.service.LoginCommandService;
 import com.wordsassemblyapi.infrastructure.author.dto.FindAuthorDto;
 import com.wordsassemblyapi.presentation.login.dto.LoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.Errors;
@@ -14,6 +13,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
@@ -48,14 +50,18 @@ public class LoginController {
     BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     FindAuthorDto dto = authorQueryService.findAuthorByEmail(request.getEmail());
 
+    Map<String, String> response = new HashMap<>();
     if (dto == null) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+      response.put("message", "user.not.found");
+      return ResponseEntity.badRequest().body(response);
     }
 
     if (!passwordEncoder.matches(request.getPassword(), dto.getPassword())) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+      response.put("message", "password.is.not.match");
+      return ResponseEntity.badRequest().body(response);
     }
 
-    return ResponseEntity.status(HttpStatus.OK).build();
+    response.put("message", "success");
+    return ResponseEntity.ok(response);
   }
 }
